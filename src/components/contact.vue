40052bfcf389732959dc4e050d49eea3c45f69a0
11 <841850130@qq.com>
2019-06-21 20:22:31 +0800

    '增加客服多语言配置项'

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -20,17 +20,10 @@
 <template>
   <div id="contact">
     <p :style='{color: color}'>{{$t("service")}}</p>
-    <div v-if='$store.state.lang == 1'>
-      <img src='../assets/ic_bottom_fb.png' />
-    </div>
-    <div v-if='$store.state.lang == 2'>
-      <img src='../assets/ic_bottom_fb.png' />
-      <img src='../assets/ic_bottom_ins.png' />
-    </div>
-    <div v-if='$store.state.lang == 3'>
-      <img src='../assets/ic_bottom_fb.png' />
-      <img src='../assets/ic_bottom_ins.png' />
-      <img src='../assets/ic_bottom_whatsapp.png' />
+    <div>
+      <a :href='$t("contacthref1")'><img src='../assets/ic_bottom_fb.png' /></a>
+      <a :href='$t("contacthref2")'><img src='../assets/ic_bottom_ins.png' /></a>
+      <a :href='$t("contacthref3")'><img src='../assets/ic_bottom_whatsapp.png' /></a>
     </div>
   </div>
 </template>
